4eabe3ec9877de300d1343b19a5df1fb81ca5353
Rich Wareham <rich@richwareham.com>
2017-12-22 12:35:11 +0000

    add "collectstatic" tox environment
    Add tox configuration to build static files into build/static.

--- smswebapp/settings/tox.py
+++ smswebapp/settings/tox.py
@@ -4,6 +4,8 @@ specific to the test suite environment. The default ``tox`` test environment
 uses this settings module when running the test suite.
 
 """
+import os
+
 # Import settings from the base settings file
 from .base import *  # noqa: F401, F403
 
@@ -11,3 +13,7 @@ from .base import *  # noqa: F401, F403
 #: The default test runner is changed to one which captures stdout and stderr
 #: when running tests.
 TEST_RUNNER = 'smswebapp.test.runner.BufferedDiscoverRunner'
+
+#: Static files are collected into a directory determined by the tox
+#: configuration. See the tox.ini file.
+STATIC_ROOT = os.environ.get('TOX_STATIC_ROOT')
