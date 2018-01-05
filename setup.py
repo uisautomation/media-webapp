518be4b73719879a38a36122aa1d62102dc74733
Rich Wareham <rjw57@cam.ac.uk>
2018-01-05 13:54:41 +0000

    add jwplatform module as a dependency
    The jwplatform integration will use the jwplatform Python module.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='smswebapp',
     packages=find_packages(),
     install_requires=[
-        'django>=2.0',
         'django-automationcommon>=1.14',
         'django-ucamwebauth>=1.4.5',
+        'django>=2.0',
+        'jwplatform',
         'psycopg2',
     ],
 )
