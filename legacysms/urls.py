43786eb1523a6117563643e5d1c5bb436097ecf2
Rich Wareham <rjw57@cam.ac.uk>
2018-04-20 11:46:45 +0100

    legacysms: change the embed URL to match the legacy SMS
    It is neater if the URL scheme under /legacy/... is the same as the
    existing SMS. That way we can use a simple redirect configuration to
    move over to using this webapp as a new provider.
    
    The existing embed URLs had a mismatch: on the SMS they were of the form
    /media/<id>/embed and on our webapp they were of the form
    /legacy/embed/<id>. Change the latter to match the former. We
    temporarily keep the older form in urlpatterns with a comment noting
    that it can be removed when the old SMS is updated.

--- legacysms/urls.py
+++ legacysms/urls.py
@@ -10,6 +10,9 @@ from . import views
 app_name = "legacy-sms"
 
 urlpatterns = [
-    path('embed/<int:media_id>/', views.embed, name='embed'),
+    # remove this once legacy SMS has the new redirect rule
+    path('embed/<int:media_id>/', views.embed),
+
+    path('media/<int:media_id>/embed', views.embed, name='embed'),
     path('rss/media/<int:media_id>/', views.rss_media, name='rss_media'),
 ]
