038ecfe8553c9d72b927662d23f10debd803644c
Rich Wareham <rjw57@cam.ac.uk>
2018-07-24 15:29:30 +0100

    frontend: switch to using react router library
    Instead of rolling our own routing solution, use react router.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import IndexPage from './pages/IndexPage';
 import MediaPage from './pages/MediaPage';
 
-const mappings = {
-  'media': <MediaPage/>,
-  'root': <IndexPage/>,
-};
-
-for (const id in mappings) {
-  if (mappings.hasOwnProperty(id)) {
-    const element = document.getElementById(id);
-    if (element) {
-      ReactDOM.render(mappings[id], element as HTMLElement);
-    }
-  }
-}
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact={true} path="/" component={IndexPage} />
+      <Route exact={true} path="/media/:pk" component={MediaPage} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('app')
+);
