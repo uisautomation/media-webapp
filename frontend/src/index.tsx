726d69e4f7ae58fa00e8eb65ad2558eeb9a3aca2
Rich Wareham <rjw57@cam.ac.uk>
2018-07-26 15:05:28 +0100

    frontend: add route which renders /upload using UploadPage
    Add a new route which uses UploadPage to render the /upload route.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import IndexPage from './pages/IndexPage';
 import MediaPage from './pages/MediaPage';
+import UploadPage from './pages/UploadPage';
 
 ReactDOM.render(
   <BrowserRouter>
     <div>
       <Route exact={true} path="/" component={IndexPage} />
       <Route exact={true} path="/media/:pk" component={MediaPage} />
+      <Route exact={true} path="/upload" component={UploadPage} />
     </div>
   </BrowserRouter>,
   document.getElementById('app')
