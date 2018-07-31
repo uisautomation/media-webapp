a84bf68193b62287d7f28f2e9b8b7661e2e2dccf
Rich Wareham <rjw57@cam.ac.uk>
2018-07-31 10:45:55 +0100

    mediaplatform: add link from media items to channel
    Add a new ForeignKey field to MediaItem which links each item to a
    channel. We add the concept of the "orphan" channel with a special id
    which collects all media items which do not otherwise have a channel. In
    practice, the number of items in this channel should be very small
    indeed.

--- mediaplatform/models.py
+++ mediaplatform/models.py
@@ -32,6 +32,18 @@ def _make_token():
 #: ids used by YouTube which suggests there'll be enough entropy for the time being.
 _TOKEN_LENGTH = len(_make_token())
 
+#: Special id reserved for the orphan channel.
+_ORPHAN_CHANNEL_ID = '!!ORPHAN!!'
+
+
+def get_orphan_channel_id():
+    """
+    Get the id of the dedicated channel for "orphaned" media items. The channel is created if if
+    does not exist.
+
+    """
+    return Channel.objects.get_or_create(id=_ORPHAN_CHANNEL_ID, title='Orphaned media items')[0].id
+
 
 class PermissionQuerySetMixin:
     def _permission_condition(self, fieldname, user):
@@ -183,6 +195,11 @@ class MediaItem(models.Model):
     #: Primary key
     id = models.CharField(
         max_length=_TOKEN_LENGTH, primary_key=True, default=_make_token, editable=False)
+
+    #: Channel which contains media item
+    channel = models.ForeignKey(
+        'Channel', help_text='Channel containing media item', default=get_orphan_channel_id,
+        on_delete=models.SET_DEFAULT, related_name='items')
 
     #: Media item title
     title = models.TextField(help_text='Title of media item', blank=True, default='')
