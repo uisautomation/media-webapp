48fb7cb8c1556941eac6169153e18dacd45c66f6
Rich Wareham <rjw57@cam.ac.uk>
2018-08-06 13:55:58 +0100

    jwpfetch: synchronise channel resources
    Use the new functionality in sync to update channel resources from JWP
    and to synchronise them to the database.

--- smsjwplatform/management/commands/jwpfetch.py
+++ smsjwplatform/management/commands/jwpfetch.py
@@ -38,8 +38,21 @@ class Command(BaseCommand):
             '--sync-all', action='store_true', dest='sync_all',
             help='Synchronise all objects from JWP cache, not only those which have been updated')
         parser.add_argument(
+            '--sync-all-videos', action='store_true', dest='sync_all_videos',
+            help='Synchronise all videos from JWP cache, not only those which have been updated')
+        parser.add_argument(
+            '--sync-all-channels', action='store_true', dest='sync_all_channels',
+            help='Synchronise all channels from JWP cache, not only those which have been updated')
+        parser.add_argument(
             '--skip-fetch', action='store_true', dest='skip_fetch',
-            help='Do not re-fetch objects from JWP, only synchronise database objects with cache')
+            help=('Do not re-fetch any resources from JWP, only synchronise database '
+                  'objects with cache'))
+        parser.add_argument(
+            '--skip-video-fetch', action='store_true', dest='skip_video_fetch',
+            help='Do not re-fetch videos from JWP, only synchronise database objects with cache')
+        parser.add_argument(
+            '--skip-channel-fetch', action='store_true', dest='skip_channel_fetch',
+            help='Do not re-fetch channels from JWP and synchronise with channels')
 
     @transaction.atomic
     def handle(self, *args, **options):
@@ -47,7 +60,7 @@ class Command(BaseCommand):
         self.client = jwplatform.get_jwplatform_client()
 
         # Fetch and cache the video resources
-        if not options['skip_fetch']:
+        if not options['skip_video_fetch'] and not options['skip_fetch']:
             self.stdout.write('Caching video resources...')
             models.set_resources(self.fetch_videos(), 'video')
 
@@ -56,12 +69,29 @@ class Command(BaseCommand):
             models.CachedResource.videos.count()
         )))
 
-        # Synchronise cached resources into main application state
-        sync.update_related_models_from_cache(options['sync_all'])
+        if not options['skip_channel_fetch'] and not options['skip_fetch']:
+            self.stdout.write('Fetching channels...')
+            models.set_resources(self.fetch_channels(), 'channel')
 
         # Print out the total number of videos cached
+        self.stdout.write(self.style.SUCCESS('Number of cached channel resources: {}'.format(
+            models.CachedResource.channels.count()
+        )))
+
+        # Synchronise cached resources into main application state
+        sync.update_related_models_from_cache(
+            update_all_videos=options['sync_all'] or options['sync_all_videos'],
+            update_all_channels=options['sync_all'] or options['sync_all_channels']
+        )
+
+        # Print out the total number of media items
         self.stdout.write(self.style.SUCCESS('Number of media items: {}'.format(
             mediaplatform.models.MediaItem.objects.count()
+        )))
+
+        # Print out the total number of videos cached
+        self.stdout.write(self.style.SUCCESS('Number of channels: {}'.format(
+            mediaplatform.models.Channel.objects.count()
         )))
 
     def fetch_videos(self):
@@ -69,10 +99,27 @@ class Command(BaseCommand):
         Returns an iterable of dicts representing all video resources in the JWPlatform database.
 
         """
+        return self._fetch_list(self.client.videos.list, 'videos')
+
+    def fetch_channels(self):
+        """
+        Returns an iterable of dicts representing all channel resources in the JWPlatform database.
+
+        """
+        return self._fetch_list(self.client.channels.list, 'channels')
+
+    def _fetch_list(self, list_callable, results_key):
+        """
+        Returns an iterable of dicts representing all resources in the JWPlatform database returned
+        by a given callable.
+
+        """
         current_offset = 0
         while True:
-            results = self.client.videos.list(
-                result_offset=current_offset, result_limit=1000).get('videos', [])
+            # We fetch only manual channels since those are the ones we sync via sms2jwplayer.
+            results = list_callable(
+                types_filter='manual',
+                result_offset=current_offset, result_limit=1000).get(results_key, [])
             current_offset += len(results)
 
             # Stop when we get no results
