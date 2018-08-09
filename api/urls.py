7c4f9d778cecf7d781afce9d8a775e366edc36af
Rich Wareham <rjw57@cam.ac.uk>
2018-08-09 13:20:19 +0100

    ensure that the swagger basePath is set correctly
    Annoyingly, drf-yasg makes the implicit assumption that the entirety of
    your application is pure API. To that end it examines *all* the URL
    paths when determining the basePath and not simply the ones that end up
    being documented.
    
    This manifests itself in our app with all of the endpoints being lumped
    together under the "api" resource. This is not only ugly, it does not
    actually reflect the reality of our API.
    
    This commit fixes this with a little bit of a work-around by splitting
    the API-related top-level urlconf into its own module and telling the
    schema generator that it is the only urlconf it need worry about. Now,
    when the swagger documentation is accessed, resources are correctly
    split into their own sections.

--- api/urls.py
+++ api/urls.py
@@ -3,26 +3,11 @@ URL routing schema for API
 
 """
 
-from django.urls import path, re_path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import path
 
 from . import views
 
 app_name = 'api'
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title='Media API',
-      default_version='v1',
-      description='Media Service Content API',
-      contact=openapi.Contact(email='automation@uis.cam.ac.uk'),
-      license=openapi.License(name='MIT License'),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('media/', views.MediaItemListView.as_view(), name='media_list'),
@@ -33,7 +18,4 @@ urlpatterns = [
     path('channels/', views.ChannelListView.as_view(), name='channel_list'),
     path('channels/<pk>', views.ChannelView.as_view(), name='channel'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    re_path(
-        r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=None), name='schema-json'),
 ]
