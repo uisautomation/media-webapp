f12a16dbe08c417e108d7aed6c33b8f7121f53bc
msb <mike@msb.me.uk>
2018-08-14 14:05:42 +0100

    created MediaEditPage and route to allow the editing of the media item's metadata

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@ import AnalyticsPage from './pages/AnalyticsPage';
 import ChannelPage from './pages/ChannelPage';
 import IndexPage from './pages/IndexPage';
 import MediaPage from './pages/MediaPage';
+import MediaEditPage from './pages/MediaEditPage';
 import UploadPage from './pages/UploadPage';
 
 ReactDOM.render(
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Route exact={true} path="/" component={IndexPage} />
       <Route exact={true} path="/media/:pk" component={MediaPage} />
       <Route exact={true} path="/media/:pk/analytics" component={AnalyticsPage} />
+      <Route exact={true} path="/media/:pk/edit" component={MediaEditPage} />
       <Route exact={true} path="/upload" component={UploadPage} />
       <Route exact={true} path="/channels/:pk" component={ChannelPage} />
     </div>
---
+++ frontend/src/pages/MediaEditPage.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+import Page from '../containers/Page';
+import ItemMetadataForm from "../components/ItemMetadataForm";
+import {mediaGet, mediaPatch} from "../api";
+
+/**
+ * A page which allows the user to edit a media item's metadata.
+ */
+class MediaEditPage extends Component {
+  constructor({ match: { params: { pk } } }) {
+    super();
+
+    // remember the media item's key for convenience
+    this.pk = pk;
+
+    this.state = {
+      // The media item being edited by the ItemMetadataForm.
+      item: {},
+      // An error object as returned by the API or the empty object if there are no errors.
+      errors: {},
+    };
+  }
+
+  /**
+   * Retrieve the item.
+   */
+  componentWillMount() {
+    mediaGet(this.pk).then(item => this.setState({ item }));
+  }
+
+  /**
+   * Save the edited item.
+   */
+  save() {
+    mediaPatch(this.state.item)
+      .then(savedItem => { location = '/media/' + this.pk })
+      .catch(({ body }) => this.setState({ errors: body }));
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { item, errors } = this.state;
+    return (
+      <Page>
+        <section className={classes.section}>
+          <Grid container justify='center'>
+            <Grid item xs={12} sm={10} md={8} lg={6}>
+              <ItemMetadataForm
+                item={item}
+                errors={errors}
+                onChange={patch => this.setState({item: {...item, ...patch}})}
+              />
+              <div className={ classes.buttonSet }>
+                <Button variant='outlined' href={ '/media/' + this.pk } >
+                  Cancel
+                </Button>
+                <Button color='primary' variant='outlined' onClick={ () => this.save() } >
+                  Save
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </section>
+      </Page>
+    );
+  }
+}
+
+const styles = theme => ({
+  section: {
+    marginTop: theme.spacing.unit,
+  },
+  buttonSet: {
+    '& button': {
+      marginLeft: theme.spacing.unit,
+    },
+    marginTop: theme.spacing.unit,
+    textAlign: 'right',
+  },
+});
+
+export default withStyles(styles)(MediaEditPage);
