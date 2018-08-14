869da3c7077dc7f777002635b2692086f9a4b1f8
Rich Wareham <rjw57@cam.ac.uk>
2018-08-14 17:01:07 +0100

    ui: re-work about page
    Re-work the about page as a single markdown document and make use of the
    new StaticTextPage component to render it.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,7 @@ import ChannelPage from './pages/ChannelPage';
 import IndexPage from './pages/IndexPage';
 import MediaPage from './pages/MediaPage';
 import PlaylistPage from './pages/PlaylistPage';
+import StaticTextPage from './pages/StaticTextPage';
 import UploadPage from './pages/UploadPage';
 
 ReactDOM.render(
@@ -19,6 +20,7 @@ ReactDOM.render(
       <Route exact={true} path="/upload" component={UploadPage} />
       <Route exact={true} path="/channels/:pk" component={ChannelPage} />
       <Route exact={true} path="/playlists/:pk" component={PlaylistPage} />
+      <Route exact={true} path="/about" component={StaticTextPage} />
     </div>
   </BrowserRouter>,
   document.getElementById('app')
