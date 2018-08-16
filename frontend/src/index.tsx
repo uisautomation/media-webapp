84042ee50fef5b61785c7da9a73ff757487c8889
msb <mike@msb.me.uk>
2018-08-16 16:21:16 +0100

    added client route and react component page to handle the creation of a playlist

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,7 @@ import ChannelPage from './pages/ChannelPage';
 import IndexPage from './pages/IndexPage';
 import MediaEditPage from './pages/MediaEditPage';
 import MediaPage from './pages/MediaPage';
+import PlaylistCreatePage from './pages/PlaylistCreatePage';
 import PlaylistPage from './pages/PlaylistPage';
 import StaticTextPage from './pages/StaticTextPage';
 import UploadPage from './pages/UploadPage';
@@ -21,6 +22,7 @@ ReactDOM.render(
       <Route exact={true} path="/media/:pk/edit" component={MediaEditPage} />
       <Route exact={true} path="/upload" component={UploadPage} />
       <Route exact={true} path="/channels/:pk" component={ChannelPage} />
+      <Route exact={true} path="/create_playlist" component={PlaylistCreatePage} />
       <Route exact={true} path="/playlists/:pk" component={PlaylistPage} />
       <Route exact={true} path="/about" component={StaticTextPage} />
     </div>
---
+++ frontend/src/pages/PlaylistCreatePage.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Grid from '@material-ui/core/Grid';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import Page from '../containers/Page';
+import ItemMetadataForm from "../components/ItemMetadataForm";
+import {withProfile} from "../providers/ProfileProvider";
+import {playlistCreate} from "../api";
+import {setMessageForNextPageLoad} from "../containers/Snackbar";
+
+/**
+ * A page which allows the user to create a new playlist.
+ */
+class PlaylistCreatePageContents extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // An error object as returned by the API or the empty object if there are no errors.
+      errors: {},
+      // The playlist being created.
+      playlist: {},
+    };
+  }
+
+  /**
+   * Create the playlist.
+   */
+  create() {
+    playlistCreate(this.state.playlist)
+      .then(playlist => {
+        setMessageForNextPageLoad('The playlist has been created.');
+        window.location = '/playlists/' + playlist.id
+      })
+      .catch(({ body }) => this.setState({ errors: body })
+    );
+  }
+
+  render() {
+    const { profile, classes } = this.props;
+    const { playlist, errors } = this.state;
+    const channels = profile ? profile.channels : [];
+
+    // only shown the form if the user has any channel with edit rights.
+    if (channels.length === 0) {
+      return (
+        <Typography variant="headline" component="div" className={classes.heading}>
+          You have no channels in which create a playlist.
+        </Typography>
+      );
+    }
+
+    return (
+      <section className={classes.section}>
+        <Grid container justify='center'>
+          <Grid item xs={12} sm={10} md={8} lg={6}>
+            <Typography variant="headline" component="div" className={classes.heading}>
+              Create a new playlist.
+            </Typography>
+            {/* TODO componentise this */}
+            <FormControl fullWidth error={ !!errors.channelId }>
+              <InputLabel htmlFor="playlist-channelId">Channel</InputLabel>
+              <Select
+                value={ playlist.channelId ? playlist.channelId : '' }
+                onChange={
+                  event => this.setState({playlist: {...playlist, channelId: event.target.value}})
+                }
+                inputProps={{
+                  id: 'playlist-channelId',
+                }}
+              >
+                {
+                  channels.map(channel => (
+                    <MenuItem key={ channel.id } value={ channel.id }>
+                      { channel.title }
+                    </MenuItem>
+                  ))
+                }
+              </Select>
+              <FormHelperText>{ errors.channelId ? errors.channelId.join(' ') : null }</FormHelperText>
+            </FormControl>
+            <ItemMetadataForm
+              item={playlist}
+              errors={errors}
+              onChange={patch => this.setState({playlist: {...playlist, ...patch}})}
+            />
+            <div className={ classes.buttonSet }>
+              <Button variant='outlined' href='/' >Cancel</Button>
+              <Button color='secondary' variant='contained' onClick={ () => this.create() } >
+                Create
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+      </section>
+    );
+  }
+}
+
+const styles = theme => ({
+  buttonSet: {
+    '& button': {
+      marginLeft: theme.spacing.unit,
+    },
+    marginTop: theme.spacing.unit,
+    textAlign: 'right',
+  },
+  heading: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+  section: {
+    marginTop: theme.spacing.unit,
+  },
+});
+
+const ConnectedPlaylistCreatePageContents = withStyles(styles)(withProfile(PlaylistCreatePageContents));
+
+const PlaylistCreatePage = () => (
+  <Page>
+    <ConnectedPlaylistCreatePageContents />
+  </Page>
+);
+
+export default PlaylistCreatePage;
