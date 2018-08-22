8e0cb5bea37772ebcee6546bdadfb496cb2ea0d5
msb <mike@msb.me.uk>
2018-08-22 10:59:56 +0100

    Implemented the ability for a suitably permissioned user to delete a playlist. The user is first presented with a confirmation modal.

--- frontend/src/pages/PlaylistPage.js
+++ frontend/src/pages/PlaylistPage.js
@@ -5,12 +5,15 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-import { playlistGet, mediaResourceToItem } from '../api';
+import { playlistGet, playlistDelete, mediaResourceToItem } from '../api';
 import MediaList from '../components/MediaList';
 import RenderedMarkdown from '../components/RenderedMarkdown';
 import Page from "../containers/Page";
 import RequiresEdit from "../containers/RequiresEdit";
+import DeletePlaylistDialog from "../containers/DeletePlaylistDialog";
+import {setMessageForNextPageLoad} from "../containers/Snackbar";
 
 /**
  * A list of media for a playlist. Upon mount, it fetches the playlist with a list of the
@@ -21,6 +24,8 @@ class PlaylistPage extends Component {
     super(props);
 
     this.state = {
+      // Controls the visibility of the delete confirmation dialog.
+      deleteDialogOpen: false,
       // The playlist resource
       playlist: null,
     }
@@ -28,29 +33,49 @@ class PlaylistPage extends Component {
 
   componentWillMount() {
     // As soon as the index page mounts, fetch the playlist.
-    const { match: { params: { pk } } } = this.props;
-    playlistGet(pk)
+    playlistGet(this.getPlaylistId())
       .then(playlist => {
         this.setState({ playlist });
       });
   }
 
+  /** Handles the user's confirmation of the playlist deletion */
+  handleConfirmDelete = (doDelete) => {
+    this.setState({deleteDialogOpen: false});
+    if (doDelete) {
+      playlistDelete(this.getPlaylistId())
+        .then(() => {
+          setMessageForNextPageLoad(`Playlist "${this.state.playlist.title}" deleted.`);
+          window.location = '/'
+        })
+        .catch(({ body }) => this.setState({ errors: body }));
+    }
+  };
+
+  /** Gets the playlist's id. */
+  getPlaylistId = () => this.props.match.params.pk;
+
   render() {
-    const { playlist } = this.state;
+    const { playlist, deleteDialogOpen } = this.state;
     return (
       <Page>
       {
         playlist !== null
         ?
-        <MediaListSection
-          playlist={ playlist }
-          MediaListProps={{
-            contentLoading: false,
-            maxItemCount: 18,
+        <div>
+          <MediaListSection
+            handleDelete={() => this.setState({deleteDialogOpen: true})}
+            playlist={ playlist }
+            MediaListProps={{
+              contentLoading: false,
+              maxItemCount: 18,
 
-            mediaItems: playlist.media.map(mediaResourceToItem),
-          }}
-        />
+              mediaItems: playlist.media.map(mediaResourceToItem),
+            }}
+          />
+          <DeletePlaylistDialog isOpen={deleteDialogOpen} title={playlist.title}
+                                handleConfirmDelete={this.handleConfirmDelete}/>
+        </div>
         :
         null
       }
@@ -73,11 +98,18 @@ const mediaListSectionStyles = theme => ({
   toolbar: {
     paddingLeft: 0,
   },
+  buttons: {
+    display: 'inline-flex',
+    '& a': {
+      marginRight: theme.spacing.unit,
+    },
+  }
 });
 
 /** A section of the body with a heading and a MediaList. */
 const MediaListSection = withStyles(mediaListSectionStyles)(({
-  classes, playlist: { id, channel, title, description }, MediaListProps, ...otherProps
+  classes, handleDelete, playlist: { id, channel, title, description },
+  MediaListProps, ...otherProps
 }) => {
   return (
     <section className={classes.root} {...otherProps}>
@@ -85,12 +117,16 @@ const MediaListSection = withStyles(mediaListSectionStyles)(({
         <Typography variant='display1' className={classes.title}>
           {title}
         </Typography>
-        <RequiresEdit channel={channel}>
+        <RequiresEdit channel={channel} className={classes.buttons}>
           <Button component='a' color='primary' variant='contained'
                   href={'/playlists/' + id + '/edit'}
           >
             Edit
             <EditIcon className={classes.rightIcon}/>
+          </Button>
+          <Button color='primary' variant='contained' onClick={handleDelete}>
+            Delete
+            <DeleteIcon className={classes.rightIcon}/>
           </Button>
         </RequiresEdit>
       </Toolbar>
