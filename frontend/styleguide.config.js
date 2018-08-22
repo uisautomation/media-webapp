adf9f57769ad07c4a2347b06042a6309fab509db
Rich Wareham <rjw57@cam.ac.uk>
2018-08-22 12:15:56 +0100

    frontend: update styleguidist config
    It turns out that changes to the styleguidist config was not noted
    because only the src/ and public/ directories from frontend were added
    to the container. Since styleguidist doesn't modify the local
    filesystem, we don't need to separate the mounts like frontend_watch so
    replace them with a single volume pointing to frontend/.
    
    Actually reading the config caused styleguidist to output some warnings
    about deprecated configuration so fix those as well.

--- frontend/styleguide.config.js
+++ frontend/styleguide.config.js
@@ -6,11 +6,10 @@
 
 module.exports = {
   // For the moment we aren't using any typescript features for components. Once we do so,
-  // uncomment the following to allow styleguidist to parse them.
-  //
-  // propsParser: require('react-docgen-typescript').parse,
+  // add the following to allow styleguidist to parse them.
+  // propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
 
-  showCode: true,
-  showUsage: true,
+  exampleMode: 'expand',
+  usageMode: 'expand',
   webpackConfig: require('react-scripts-ts/config/webpack.config.dev.js'),
 };
