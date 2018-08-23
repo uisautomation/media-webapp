f48504c554ca61bb4775daa4316843efa3da94c5
Rich Wareham <rjw57@cam.ac.uk>
2018-08-23 10:14:38 +0100

    frontend: move MotdBanner over to being a BodySection
    The MotdBanner component is now based on BodySection so that its left
    and right padding responds to screen size.

--- frontend/src/components/MotdBanner.js
+++ frontend/src/components/MotdBanner.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
+import BodySection from '../components/BodySection';
+
 import ReleaseTag from './ReleaseTag';
 
 /**
@@ -14,31 +16,28 @@ import ReleaseTag from './ReleaseTag';
 const MotdBanner = ({
   classes, component: Component, ...otherProps
 }) => (
-  <Component component='div' className={classes.root} {...otherProps}>
-    <div className={classes.message}>
+  <BodySection classes={{ root: classes.root }}>
+    <Typography component='div' classes={{ root: classes.message }}>
       <ReleaseTag style={{marginRight: '0.5ex'}}>alpha</ReleaseTag>
       {' '}This service is in development.{' '} { /* whitespace coalescing in JSX sux! */ }
       <a className={classes.link} href="/about#help-us">Help us improve it.</a>
-    </div>
-  </Component>
+    </Typography>
+  </BodySection>
 )
 
 MotdBanner.propTypes = {
-  /** Base component for the control. */
-  component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
 };
 
 MotdBanner.defaultProps = {
-  component: Typography,
 };
 
 const styles = theme => ({
   root: {
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.08)',
+    borderBottom: [[1, 'solid', theme.palette.divider]],
     display: 'flex',
     ...theme.mixins.toolbar,
-    ...theme.mixins.bodySection,
   },
 
   message: { /* No default styles */ },
