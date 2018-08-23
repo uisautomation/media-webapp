e06532a6bdf1881c63ee8c9a594e2a2b91e52cd9
Rich Wareham <rjw57@cam.ac.uk>
2018-08-23 09:39:59 +0100

    frontend: add new component BodySection
    Add a new component BodySection which is to be used for sections of body
    text whose padding needs to adapt to screen size. The padding is
    consistent with the padding in the toolbar meaning that vertical edges
    of body components will line up visually with toolbar elements.

---
+++ frontend/src/components/BodySection.md
@@ -0,0 +1,18 @@
+## Examples
+
+```js
+<div style={{backgroundColor: 'red'}}>
+    <BodySection style={{backgroundColor: 'green'}}>
+        section
+    </BodySection>
+</div>
+```
+
+## CSS API
+
+You can override the injected class names with the ``classes`` property. This
+property accepts the following keys:
+
+* ``root``
+
+To override this component in themes use the name ``BodySection``.
---
+++ frontend/src/components/BodySection.tsx
@@ -0,0 +1,68 @@
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import createStyles from '@material-ui/core/styles/createStyles';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2,
+
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: theme.spacing.unit * 3,
+      paddingRight: theme.spacing.unit * 2,
+    },
+  },
+
+  gutterBottom: {
+    marginBottom: theme.spacing.unit * 2,
+  },
+});
+
+export interface IProps extends WithStyles<typeof styles> {
+  children: React.ReactNode[];
+
+  component: React.ReactType;
+
+  gutterBottom: boolean;
+
+  // Indicates that unknown props are also accepted.
+  [prop: string]: any;
+}
+
+/**
+ * A ``<section>``-like component which defaults to the correct left/right padding for body text
+ * within the application. Use instead of ``<section>`` where you want consistent left/right
+ * margins for the element.
+ *
+ * Unknown props (including ``classes``) are broadcast to the root element.
+ */
+const BodySection: React.SFC<IProps> = (
+  { classes, children, component: Component, gutterBottom, ...otherProps }
+) => (
+  <Component
+    className={ [classes.root, gutterBottom ? classes.gutterBottom : ''].join(' ') }
+    classes={classes} {...otherProps}
+  >
+    { children }
+  </Component>
+);
+
+// Until we're entirely typescript, we still need propTypes to do runtime type checking.
+
+BodySection.propTypes = {
+  /** Component used for the section */
+  component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+
+  /** Should extra margin be added to the bottom? */
+  gutterBottom: PropTypes.bool,
+};
+
+BodySection.defaultProps = {
+  component: 'section',
+  gutterBottom: false,
+};
+
+export default withStyles(styles, { name: 'BodySection' })(BodySection);
