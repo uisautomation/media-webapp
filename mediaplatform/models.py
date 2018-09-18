4820667094f266528ea62f414903e63e833d94bc
Rich Wareham <rjw57@cam.ac.uk>
2018-09-18 13:33:42 +0100

    mediaplatform: code style fix
    Move import from top of file into the non-system modules import pack.

--- mediaplatform/models.py
+++ mediaplatform/models.py
@@ -1,5 +1,3 @@
-from django.utils import timezone
-
 import dataclasses
 import itertools
 import secrets
@@ -12,6 +10,7 @@ from django.db import models
 from django.db.models import Q
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 from django.utils.functional import cached_property
 from iso639 import languages
 
