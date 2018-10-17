62c267a9bcb78285b3d3599124adfc65bf0dffc3
Rich Wareham <rich@richwareham.com>
2018-10-17 21:12:05 +0100

    replace legacy download with redirect to new site
    Use the same strategy we use for media pages and for embed links for
    download links. This somehow got overlooked in the move away from
    putting JWP APIs on our critical path.

--- legacysms/tests/test_views.py
+++ legacysms/tests/test_views.py
@@ -2,23 +2,19 @@
 Tests for views.
 
 """
-import copy
 from contextlib import contextmanager
 from unittest import mock
 
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.test import TestCase
-import requests
 
-import mediaplatform_jwp.api.delivery as api
 from mediaplatform_jwp.models import CachedResource
 from mediaplatform import models as mpmodels
 from mediaplatform_jwp import sync
 
 from .. import models
 from .. import redirect
-from .. import views
 
 
 class TestCaseWithFixtures(TestCase):
@@ -97,178 +93,27 @@ class RSSMediaTestCase(TestCaseWithFixtures):
         self.assertEqual(r.status_code, 404)
 
 
-class DownloadTests(TestCase):
+class DownloadTests(TestCaseWithFixtures):
     """
     Test media download functionality.
 
     """
 
-    def setUp(self):
-        # Patch the video_from_media_id call
-        self.video_from_media_id_patcher = mock.patch(
-            'mediaplatform_jwp.api.delivery.Video.from_media_id')
-
-        # Patch the requests library
-        self.requests_session_patcher = mock.patch('legacysms.views.DEFAULT_REQUESTS_SESSION')
-
-        self.video_from_media_id = self.video_from_media_id_patcher.start()
-        self.requests_session = self.requests_session_patcher.start()
-
-        # Video.from_media_id() by default returns a mock Video resource
-        self.mock_video = api.Video({'key': 'video-key'})
-        self.video_from_media_id.return_value = self.mock_video
-
-    def tearDown(self):
-        # Stop patching
-        self.video_from_media_id_patcher.stop()
-        self.requests_session_patcher.stop()
-
     def test_basic_functionality(self):
         """Basic redirection functionality works."""
-        self.requests_session.get.return_value.json.return_value = MEDIA_INFO
-
         r = self.client.get(reverse('legacysms:download_media',
                                     kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
+        item = mpmodels.MediaItem.objects.get(sms__id=34)
 
-        self.assertRedirects(r, 'http://media.invalid/2.mp4', fetch_redirect_response=False)
+        self.assertRedirects(
+            r, reverse('api:media_source', kwargs={'pk': item.id}),
+            fetch_redirect_response=False)
 
-    def test_passes_video_key_to_jwp(self):
-        """The correct video key used to get video info."""
-        with mock.patch('time.time', return_value=12345):
-            self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-
-            self.requests_session.get.assert_called_once_with(
-                api.pd_api_url(f'/v2/media/video-key', format='json'), timeout=5
-            )
-
-    def test_redirects_if_not_found(self):
-        """Redirects to legacy SMS if video is not present."""
-        self.video_from_media_id.side_effect = api.VideoNotFoundError()
-
+    def test_404_if_not_found(self):
+        """Returns a 404 response if video is not present."""
         r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-    def test_bad_gateway_if_timeout(self):
-        """If request times out, a bad gateway error is returned."""
-        self.requests_session.get.side_effect = requests.Timeout()
-
-        # Check that a warning is also logged
-        with self.assertLogs(views.LOG, 'WARNING'):
-            r = self.client.get(reverse(
-                'legacysms:download_media',
-                kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-
-        self.assertEqual(r.status_code, 502)
-
-    def test_bad_gateway_if_upstream_error(self):
-        """If request to JWPlayer gives HTTP error, a bad gateway error is returned."""
-        self.requests_session.get.return_value.raise_for_status.side_effect = requests.HTTPError()
-
-        # Check that a warning is also logged
-        with self.assertLogs(views.LOG, 'WARNING'):
-            r = self.client.get(reverse(
-                'legacysms:download_media',
-                kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-
-        self.assertEqual(r.status_code, 502)
-
-    def test_bad_gateway_if_bad_json(self):
-        """If request to JWPlayer gives un-parseable JSON, a bad gateway error is returned."""
-        self.requests_session.get.return_value.json.side_effect = Exception()
-
-        # Check that a warning is also logged
-        with self.assertLogs(views.LOG, 'WARNING'):
-            r = self.client.get(reverse(
-                'legacysms:download_media',
-                kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-
-        self.assertEqual(r.status_code, 502)
-
-    def test_redirects_if_no_playlist(self):
-        """Redirects to legacy SMS if there is no playlist or an empty playlist."""
-        # No playlist
-        self.requests_session.get.return_value.json.return_value = {}
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-        # Empty playlist
-        self.requests_session.get.return_value.json.return_value = {'playlist': []}
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-    def test_redirects_if_no_sources(self):
-        """Redirects to legacy SMS if there are no sources."""
-        # No sources
-        self.requests_session.get.return_value.json.return_value = {'playlist': [{}]}
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-        # Empty sources
-        self.requests_session.get.return_value.json.return_value = {'playlist': [{'sources': []}]}
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-    def test_404_if_bad_extension(self):
-        """404 is returned if an unknown extension is given"""
-        self.requests_session.get.return_value.json.return_value = MEDIA_INFO
-
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension':
-                                            'not-a-video'}))
-
+                                    kwargs={'media_id': 3456, 'clip_id': 56, 'extension': 'mp4'}))
         self.assertEqual(r.status_code, 404)
-
-    def test_redirects_if_no_file(self):
-        """Redirects to legacy SMS if the source has no file set."""
-        media_info = copy.deepcopy(MEDIA_INFO)
-        media_info['playlist'][0]['sources'] = [
-            {'width': 1280, 'height': 720, 'type': 'video/mp4'}
-        ]
-
-        self.requests_session.get.return_value.json.return_value = media_info
-        r = self.client.get(reverse('legacysms:download_media',
-                                    kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-        self.assertRedirects(r, redirect.media_download(34, 56, 'mp4')['Location'],
-                             fetch_redirect_response=False)
-
-    def test_no_permission(self):
-        """
-        Tests the behaviour when the user's identity (if any) doesn't match the ACL.
-
-        """
-        with mock.patch('mediaplatform_jwp.api.delivery.Video.from_media_id'
-                        ) as from_media_id:
-            from_media_id.return_value = api.Video({
-                'key': 'video-key',
-                'custom': {
-                    'sms_acl': 'acl:USER_mb2174:',
-                    'sms_media_id': 'media:34:',
-                }
-            })
-            self.assertEqual(api.Video.from_media_id(34).acl, ['USER_mb2174'])
-
-            r = self.client.get(reverse(
-                'legacysms:download_media',
-                kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-            self.assertEqual(r.status_code, 403)
-
-            self.client.force_login(User.objects.create(username='spqr1'))
-            r = self.client.get(reverse(
-                'legacysms:download_media',
-                kwargs={'media_id': 34, 'clip_id': 56, 'extension': 'mp4'}))
-            self.assertEqual(r.status_code, 403)
 
 
 class MediaPageTest(TestCaseWithFixtures):
